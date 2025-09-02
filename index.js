99e5eab8f64bc9ca9e6712cf8be7bb35b39f27a3
Darly Johana Zambrano Zambrano <zambrano1985darly@gmail.com>
2025-09-01 21:15:13 -0500

    Se agrega función de resta

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ function sumar(num1, num2) {
   return num1 + num2;
 }
 
+// Función para restar dos números
+function restar(num1, num2) {
+  return num1 - num2;
+}
+
 // Función principal del programa
 function calculadora() {
   var opcion = prompt(
